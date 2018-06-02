6c0ce8ba68dbf6e891a18717528541d8733d98a2
Denis Litvin <den.litvinn@gmail.com>
2018-06-02 14:40:14 +0300

    Initial Commit

---
+++ MVVMCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMCollectionView
+//
+//  Created by Denis Litvin on 02.06.2018.
+//  Copyright © 2018 Denis Litvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
